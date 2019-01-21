37ae9d4face6b199889452894a112be396c527d0
ballin <a0955771728@gmail.com>
2019-01-21 14:23:26 +0800

    增加人物頭像，名字，講的話

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,13 +1,31 @@
-$(function () {
-    var socket = io();
-    $('form').submit(function (e) {
+$(() => {
+    const socket = io();
+    $('form').submit((e) => {
         e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
-        return false;
+        let regu = "^[ ]+$"; // regular expression
+        let re = new RegExp(regu);
+        // Returns a Boolean value that indicates whether or not a pattern exists in a searched string.
+        // 防止啥都沒打跟只打了一堆空白就送出
+        if (re.test($('#m').val()) == true || $('#m').val() == '') {
+            $('#m').val('');
+            return false;
+        }
+        else {
+            socket.emit('chat message', $('#m').val());
+            $('#m').val('');
+            return false;
+        }
     });
-    socket.on('chat message', function (msg) {
-        $('#messages').append($('<li>').text(msg));
+    socket.on('chat message', (msg) => {
+        $('#messages').append(`
+        <li>
+            <img id="userPhoto" src="../static/image/eggroll.PNG"/>
+            <span>一枚蛋捲</span>
+            <div>
+                <p>${msg}</p>
+            </div>
+        </li>`);
+        //$('#messages').append($('<li>').text(msg));
         $('#messages').scrollTop($('#messages')[0].scrollHeight); // 讓 scrollbar 一直滾到最下方。
     });
 });
